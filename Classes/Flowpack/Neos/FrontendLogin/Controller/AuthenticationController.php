8a617438a5429c1f5f375e929692f75cbdac0ac9
Bastian <b.waidelich@wwwision.de>
2017-04-12 15:10:23 +0200

    TASK: Adjust Coding style to recent CGL
    Just a cosmetic fix to make the (PHP) code adhere to
    our latest coding guidelines

--- Classes/Flowpack/Neos/FrontendLogin/Controller/AuthenticationController.php
+++ Classes/Flowpack/Neos/FrontendLogin/Controller/AuthenticationController.php
@@ -8,50 +8,55 @@ use Neos\Flow\Security\Authentication\Controller\AbstractAuthenticationControlle
 /**
  * Controller for displaying a login/logout form and authenticating/logging out "frontend users"
  */
-class AuthenticationController extends AbstractAuthenticationController {
+class AuthenticationController extends AbstractAuthenticationController
+{
 
-	/**
-	 * @return void
-	 */
-	public function indexAction() {
-		$this->view->assign('account', $this->securityContext->getAccount());
-	}
+    /**
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->view->assign('account', $this->securityContext->getAccount());
+    }
 
-	/**
-	 * return void
-	 */
-	public function logoutAction() {
-		parent::logoutAction();
+    /**
+     * return void
+     */
+    public function logoutAction()
+    {
+        parent::logoutAction();
 
-		$uri = $this->request->getInternalArgument('__redirectAfterLogoutUri');
+        $uri = $this->request->getInternalArgument('__redirectAfterLogoutUri');
 
-		if (empty($uri)) {
-			$this->redirect('index');
-		} else {
-			$this->redirectToUri($uri);
-		}
-	}
+        if (empty($uri)) {
+            $this->redirect('index');
+        } else {
+            $this->redirectToUri($uri);
+        }
+    }
 
-	/**
-	 * @param ActionRequest $originalRequest The request that was intercepted by the security framework, NULL if there was none
-	 * @return string
-	 */
-	protected function onAuthenticationSuccess(ActionRequest $originalRequest = NULL) {
-		$uri = $this->request->getInternalArgument('__redirectAfterLoginUri');
+    /**
+     * @param ActionRequest $originalRequest The request that was intercepted by the security framework, NULL if there was none
+     * @return string
+     */
+    protected function onAuthenticationSuccess(ActionRequest $originalRequest = null)
+    {
+        $uri = $this->request->getInternalArgument('__redirectAfterLoginUri');
 
-		if (empty($uri)) {
-			$this->redirect('index');
-		} else {
-			$this->redirectToUri($uri);
-		}
-	}
+        if (empty($uri)) {
+            $this->redirect('index');
+        } else {
+            $this->redirectToUri($uri);
+        }
+    }
 
-	/**
-	 * Disable the technical error flash message
-	 *
-	 * @return boolean
-	 */
-	protected function getErrorFlashMessage() {
-		return FALSE;
-	}
+    /**
+     * Disable the technical error flash message
+     *
+     * @return boolean
+     */
+    protected function getErrorFlashMessage()
+    {
+        return false;
+    }
 }
